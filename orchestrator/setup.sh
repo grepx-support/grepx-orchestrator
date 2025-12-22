b32cfa5130acd4247e2619776f361cfaaa6eedc3
Aarti Gupta <aarti.gupta@grepx.co.in>
2025-12-22 19:00:33 +0530

    Add Prefect integration to run.sh and application.py

--- orchestrator/setup.sh
+++ orchestrator/setup.sh
@@ -10,7 +10,7 @@ source ./common.sh
 
 if [ ! -d "venv" ]; then
     echo "Creating virtual environment..."
-    PYTHON=$(find_python)
+    PYTHON="/c/Users/ASUS/AppData/Local/Programs/Python/Python312/python"
     $PYTHON -m venv venv
 fi
 
